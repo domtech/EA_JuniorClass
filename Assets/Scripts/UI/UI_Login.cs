ecd059c1269386c645543ec82e99474590889d7e
holdmycat <holdmycat@163.com>
2021-03-02 11:06:56 +0800

    tur23:add login

--- Assets/Scripts/UI/UI_Login.cs
+++ Assets/Scripts/UI/UI_Login.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 public class UI_Login : UIBase
 {
 
+    Canvas CanvasInst;
+
+    private void Awake()
+    {
+        CanvasInst = GetComponent<Canvas>();
+        CanvasInst.worldCamera = Camera.main;
+    }
+
     public void OnLogin()
     {
 

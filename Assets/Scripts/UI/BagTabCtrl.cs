d423704a2db9a3d53a65d6cbb0c37a8bc4e3a988
holdmycat <holdmycat@163.com>
2021-03-04 18:51:30 +0800

    tur27:ui_bag tabs

---
+++ Assets/Scripts/UI/BagTabCtrl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BagTabCtrl : MonoBehaviour
+{
+
+    TabItem[] TabItemArray;
+
+    int tabIndex = 0;
+    public int TabIndex => tabIndex;
+
+    private void Awake()
+    {
+       
+        TabItemArray = gameObject.GetComponentsInChildren<TabItem>();
+        tabIndex = 0;
+        TabItemArray[tabIndex].SetTabItemState(true);
+    }
+
+    public void ClickTabItem(int index)
+    {
+        for(var i = 0; i < TabItemArray.Length; i++)
+        {
+            if(i == index)
+            {
+                tabIndex = i;
+                TabItemArray[i].SetTabItemState(true);
+            }
+            else
+            {
+                TabItemArray[i].SetTabItemState(false);
+            }
+        }
+    }
+
+}
